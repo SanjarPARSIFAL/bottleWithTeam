75b1fe91510640120c74da563f2e3f0460bb5fa5
SanjarPARSIFAL <sanjar666parsifal@gmail.com>
2024-02-04 19:06:55 +0500

    sanjar

--- src/uz/pdp/service/GenerateBottle.java
+++ src/uz/pdp/service/GenerateBottle.java
@@ -6,22 +6,22 @@ import uz.pdp.utils.Input;
 public class GenerateBottle {
     public static Bottle bottle = new Bottle(0);
     public static void addWater(){
-        int x = Input.INT("AmountWater : ");
+        int x = Input.INT("Amount of Water : ");
         int amount = x + bottle.getAmount();
         if(amount > bottle.getSize()){
             System.out.println("Bottle is Full !!!");
         }else{
-            System.out.println("Success addWater !!!");
+            System.out.println("success!");
             bottle.setAmount(amount);
         }
     }
     public static void pourWater(){
-        int x = Input.INT("AmountWater : ");
+        int x = Input.INT("Amount of Water : ");
         if(bottle.getAmount()<x){
             System.out.println("Try again.");
             return;
         }
         bottle.setAmount(bottle.getAmount()-x);
-        System.out.println("Success add Water !!!.");
+        System.out.println("success!");
     }
 }
