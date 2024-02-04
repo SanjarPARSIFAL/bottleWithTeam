7326928bbc3f6ed40497ab0aca1832199763b1e5
Xasanov Oybek <ox1213982@gmail.com>
2024-02-04 18:48:53 +0500

    addWater() completed

--- src/uz/pdp/service/GenerateBottle.java
+++ src/uz/pdp/service/GenerateBottle.java
@@ -1,11 +1,18 @@
 package uz.pdp.service;
 
 import uz.pdp.entity.Bottle;
+import uz.pdp.utils.Input;
 
 public class GenerateBottle {
     static Bottle bottle = new Bottle(0);
     public static void addWater(){
-
+        int x = Input.INT("amountWater : ");
+        int amount = x + bottle.getAmount();
+        if(amount > bottle.getSize()){
+            System.out.println("bottle is Ful !!!");
+        }else{
+            bottle.setAmount(amount);
+        }
     }
     public static void pourWater(){
 
