26b4d510d92e80a5a23358d84747dc24d1abed81
Diyorbek Normuhammdov <diyorbeknormuhammadov1@gmail.com>
2024-02-04 18:47:05 +0500

    pourWater

--- src/uz/pdp/service/GenerateBottle.java
+++ src/uz/pdp/service/GenerateBottle.java
@@ -1,6 +1,7 @@
 package uz.pdp.service;
 
 import uz.pdp.entity.Bottle;
+import uz.pdp.utils.Input;
 
 public class GenerateBottle {
     public static Bottle bottle = new Bottle(0);
@@ -8,6 +9,12 @@ public class GenerateBottle {
 
     }
     public static void pourWater(){
-
+        int x = Input.INT("Qancha suv to'kmoqchisiz:");
+        if(bottle.getAmount()<x){
+            System.out.println("Buncha suv mavjud emas.");
+            return;
+        }
+        bottle.setAmount(bottle.getAmount()-x);
+        System.out.println("Bajarildi.");
     }
 }
